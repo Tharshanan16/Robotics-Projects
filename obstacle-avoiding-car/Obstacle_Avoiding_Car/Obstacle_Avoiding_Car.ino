8100cfc2cd0c3bfabea73fda45e71f90605aa1e0
Tharshanan16 <VisvaranjanTharshanan@gmail.com>
2026-09-19 22:12:59 +0200

    Obstacle avoiding Car using Arduino

---
+++ obstacle-avoiding-car/Obstacle_Avoiding_Car/Obstacle_Avoiding_Car.ino
@@ -0,0 +1,131 @@
+//include the servo library
+#include <Servo.h>
+
+//Set the servo Angle
+int servoAngle=90;
+
+// Creates a servo object for controlling the servo motor
+Servo myServo; 
+
+//Define the PWM pin  which attached to Servo
+int servoPin=9;
+
+
+
+//Defines Trig and Echo pins of the UltraSonic Sensor
+const int trigPin =10;
+const int echoPin =11;
+
+
+
+//Left two motors
+int pwmA= 5;
+int in1A=3;
+int in2A=4;
+
+//Right two motors
+int pwmB=6;
+int in1B=7;
+int in2B=8;
+
+//Standby
+const int STBY=2;
+
+//MotorSpeed values
+
+int MotorSpeed =180;
+
+void setup() {
+
+ // Ultra sonic 
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+
+  //Setup the pin for the Servo
+  myServo.write(90);
+  myServo.attach(servoPin);
+
+// Set all the motor control pins to outputs
+  pinMode(pwmA,OUTPUT);
+  pinMode(in1A,OUTPUT);
+  pinMode(in2A,OUTPUT);
+
+  pinMode(pwmB,OUTPUT);
+  pinMode(in1B,OUTPUT);
+  pinMode(in2B,OUTPUT);
+
+// TB6612FNG standby
+  pinMode(STBY, OUTPUT);
+  digitalWrite(STBY, HIGH);
+
+  Serial.begin(9600);
+
+  stopMotors();
+
+  delay(1000);
+
+
+}
+
+void loop() {
+  // Look stright
+  myServo.write(90);
+  delay(200);
+
+  //Measure Distance
+  distance =getDistance();
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.print(" cm");
+
+
+
+  //If there no Obstacle
+
+  if (distance>20){
+    moveForward();
+  }
+
+  //If there Obstacle
+
+  else {
+    stopMotors();
+    delay(300);
+  }
+  
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
